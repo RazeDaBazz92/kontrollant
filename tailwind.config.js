622f243a857c216a4ea9e9b9085ee4ca46040478
rasmusthunborgRB <rasmus.thunborg@relationbrand.com>
2021-08-16 22:27:17 +0200

    fix height

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     extend: {
       gridTemplateRows: {
         '8':'repeat(8, minmax(0, 1fr))',
-        '10':'repeat(10, minmax(0, 1fr))',
+        '10':'repeat(10, minmax(0, 1fr))'
       },
       width: {
         '1/7': '14.2857143%',
@@ -14,8 +14,11 @@ module.exports = {
         '3/7': '42.8571429%',
         '4/7': '57.1428571%',
         '5/7': '71.4285714%',
-        '6/7': '85.7142857%',
-      }
+        '6/7': '85.7142857%'
+      },
+      minHeight: {
+        'webkit': '-webkit-fill-available',
+       }
     },
   },
   variants: {
